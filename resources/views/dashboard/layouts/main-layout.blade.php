7ca0e8bebdb422dcb7e64d4208e143595c68e816
Alriskhandy <alriskhandy19@gmmail.com>
2024-05-09 20:34:20 +0900

    ubah favicon & title layout

--- resources/views/dashboard/layouts/main-layout.blade.php
+++ resources/views/dashboard/layouts/main-layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Dashboard - Sistem Informasi Fakultas Teknik</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
